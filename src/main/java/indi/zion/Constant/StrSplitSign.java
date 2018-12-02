5cf312d7b398486946ecaec1365667fa9070937a
time881 <time881@163.com>
2018-12-02 21:50:53 +0800

    all text end with sign "#"

--- src/main/java/indi/zion/Constant/StrSplitSign.java
+++ src/main/java/indi/zion/Constant/StrSplitSign.java
@@ -3,6 +3,7 @@ package indi.zion.Constant;
 public class StrSplitSign {
     public static byte n = "\n".getBytes()[0];
     public static byte r = "\r".getBytes()[0];
+    public static byte end = "#".getBytes()[0];
     //line split
     public static byte ls = ":".getBytes()[0];
 }
