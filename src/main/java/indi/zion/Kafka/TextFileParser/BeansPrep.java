03004370373637f0534fa4a24ed58a94d4c2cbcc
time881 <time881@163.com>
2018-12-02 21:46:37 +0800

    Correcting some Thread pool issue

--- src/main/java/indi/zion/Kafka/TextFileParser/BeansPrep.java
+++ src/main/java/indi/zion/Kafka/TextFileParser/BeansPrep.java
@@ -16,8 +16,8 @@ import indi.zion.Util.TempClassUtil;
 /**
  * 
  * transform block to bean according to Temporary in .properties, by using
- * genericity class T invoke to Bean class and new instance. parser block with line
- * terminated by "\r|\n" and field terminated by ":"
+ * genericity class T invoke to Bean class and new instance. parser block with
+ * line terminated by "\r|\n" and field terminated by ":"
  * 
  * @author Administrator
  * @param <T>
@@ -41,14 +41,24 @@ public class BeansPrep<T extends Bean> {
                 if (Block[i] == StrSplitSign.ls && Block[i + 1] == StrSplitSign.ls) {
                     beanEnd = i;
                     Setter(bean, ValueType, no, Block, beanStart, beanEnd);
+                    while ((Block[i] == StrSplitSign.n || Block[i] == StrSplitSign.r || Block[i] == StrSplitSign.ls)) {
+                        i++;//pass when byte belong sign of split
+                        if(i == Block.length)break;
+                    }
                     beanStart = i;
                     no += 1;
                 }
-                if (Block[i] == StrSplitSign.n || Block[i] == StrSplitSign.r) {
+                if (Block[i] == StrSplitSign.n || Block[i] == StrSplitSign.r || Block[i] == StrSplitSign.end) {
                     beanEnd = i;
-                    Setter(bean, ValueType, no, Block, beanStart, beanEnd);
-                    ware.add(bean);
-                    bean = cons.newInstance();// as store into ware, then new instance
+                    if (beanEnd != beanStart) {
+                        Setter(bean, ValueType, no, Block, beanStart, beanEnd);
+                        ware.add(bean);
+                        bean = cons.newInstance();// as store into ware, then new instance
+                    }
+                    while ((Block[i] == StrSplitSign.n || Block[i] == StrSplitSign.r || Block[i] == StrSplitSign.ls)) {
+                        i++;//pass when byte belong sign of split
+                        if(i == Block.length)break;
+                    }
                     beanStart = i;
                     no = 0;
                 }
@@ -67,10 +77,10 @@ public class BeansPrep<T extends Bean> {
      */
     private void Setter(T bean, String[][] ValueType, int no, byte[] Block, int beanStart, int beanEnd)
             throws Exception {
-        byte[] tempValue = new byte[beanEnd + 1 - beanStart];
-        System.arraycopy(Block, beanStart, tempValue, 0, beanEnd + 1 - beanStart);
+        byte[] tempValue = new byte[beanEnd - beanStart];
+        System.arraycopy(Block, beanStart, tempValue, 0, beanEnd - beanStart);
         Class<?> tempType = Class.forName(TempClassUtil.SupplyClassName(ValueType[no][1]));
-        BeanClass.getMethod("set" + ValueType[no][0], tempType).invoke(bean, tempType,
+        BeanClass.getMethod("set" + ValueType[no][0], tempType).invoke(bean,
                 TempClassUtil.GetValue(tempValue, tempType));
     }
 
@@ -87,7 +97,9 @@ public class BeansPrep<T extends Bean> {
 
     private void InitBeanStruct() {
         InitProp();
-        String[] Values = props.getProperty(BeanClass.getName()).split(",");
+        System.out.println(BeanClass.getName());
+        String BeanClassName = BeanClass.getName();// package + class name
+        String[] Values = props.getProperty(BeanClassName.substring(BeanClassName.lastIndexOf(".") + 1)).split(",");
         ValueType = new String[Values.length][2];
         int i = 0;
         for (String value : Values) {
