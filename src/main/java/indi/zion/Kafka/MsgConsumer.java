13038bde19238aa6cec6c0a4ee275d415c6286b6
time881 <time881@163.com>
2018-12-12 22:32:21 +0800

    Add Kafka Topic check

--- src/main/java/indi/zion/Kafka/MsgConsumer.java
+++ src/main/java/indi/zion/Kafka/MsgConsumer.java
@@ -4,9 +4,13 @@ import java.io.InputStream;
 import java.time.Duration;
 import java.util.Arrays;
 import java.util.Properties;
+import java.util.Set;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
 
+import org.apache.kafka.clients.CommonClientConfigs;
+import org.apache.kafka.clients.admin.AdminClient;
+import org.apache.kafka.clients.admin.ListTopicsResult;
 import org.apache.kafka.clients.consumer.ConsumerRecord;
 import org.apache.kafka.clients.consumer.ConsumerRecords;
 import org.apache.kafka.clients.consumer.KafkaConsumer;
@@ -26,32 +30,49 @@ public class MsgConsumer {
             props.put("key.deserializer", StringDeserializer.class.getName());
             props.put("value.deserializer", new BeanDecoder<Rate>().getClass().getName());
             inStream.close();
+            return new KafkaConsumer<String, Rate>(props);
         } catch (Exception e) {
             // TODO Auto-generated catch block
             e.printStackTrace();
         }
-        return new KafkaConsumer<String, Rate>(props);
+        return null;
     }
     
-    public void NewThreadPool(int NumofThread) {
-        consumer = InitProp();
-        consumer.subscribe(Arrays.asList(props.getProperty("TOPIC")));
-        ExecutorService cachedThreadPool = Executors.newFixedThreadPool(NumofThread);
-        while(true) {
-            ConsumerRecords<String, Rate> consumerRecords = consumer.poll(Duration.ofSeconds(1));
-            for (ConsumerRecord consumerRecord : consumerRecords) {
-                cachedThreadPool.submit(() ->{ConsumerAction(consumerRecord);});
-                //Pending implement close Consume control
+    public void Consumer() {
+        try {
+            consumer = InitProp();
+            Properties properties = new Properties();
+            properties.put(CommonClientConfigs.BOOTSTRAP_SERVERS_CONFIG, props.getProperty("bootstrap.servers"));
+            AdminClient adminClient = AdminClient.create(properties);
+            ListTopicsResult listTopics = adminClient.listTopics();
+            Set<String> names = listTopics.names().get();
+            if(names.contains(props.getProperty("TOPIC"))) {
+                consumer.subscribe(Arrays.asList(props.getProperty("TOPIC")));
+                while(true) {
+                    try{
+                        ConsumerRecords<String, Rate> consumerRecords = consumer.poll(Duration.ofSeconds(1));
+                        for (ConsumerRecord consumerRecord : consumerRecords) {
+                            //Pending implement close Consume control
+                            ConsumerAction(consumerRecord);
+                        }
+                    }catch (Exception e) {
+                        // TODO: handle exception
+                        e.printStackTrace();
+                    }
+                }
             }
+        }catch (Exception e) {
+            // TODO: handle exception
+            e.printStackTrace();
         }
     }
     
-    public void ConsumerAction(ConsumerRecord<String, String> consumerRecord) {
+    public void ConsumerAction(ConsumerRecord<String, Rate> consumerRecord) {
         System.out.format("ThreadID:%d\t%d\t%d\t%s\t%s\n", 
             Thread.currentThread().getId(), 
             consumerRecord.offset(), 
             consumerRecord.partition(), 
             consumerRecord.key(),
-            consumerRecord.value());
+            consumerRecord.value().getMovieID());
     }
 }
