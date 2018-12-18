6a0fdaec9624aa9e2640626a140833ac848d0225
time881 <time881@163.com>
2018-12-18 20:35:04 +0800

    spark streaming application

--- src/main/java/indi/zion/Kafka/SparkStreaming/MoviesHotStatistic.java
+++ src/main/java/indi/zion/Kafka/SparkStreaming/MoviesHotStatistic.java
@@ -1,10 +1,10 @@
 package indi.zion.Kafka.SparkStreaming;
 
 import java.io.InputStream;
-import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Collection;
 import java.util.HashMap;
+import java.util.Iterator;
 import java.util.Map;
 import java.util.Properties;
 import java.util.Set;
@@ -12,14 +12,16 @@ import java.util.concurrent.ExecutionException;
 
 import org.apache.kafka.clients.CommonClientConfigs;
 import org.apache.kafka.clients.admin.AdminClient;
-import org.apache.kafka.clients.admin.ListTopicsResult;
 import org.apache.kafka.clients.consumer.ConsumerRecord;
-import org.apache.kafka.clients.consumer.KafkaConsumer;
 import org.apache.kafka.common.serialization.StringDeserializer;
 import org.apache.spark.SparkConf;
+import org.apache.spark.api.java.JavaPairRDD;
 import org.apache.spark.api.java.function.Function2;
 import org.apache.spark.api.java.function.PairFunction;
+import org.apache.spark.api.java.function.VoidFunction;
+import org.apache.spark.api.java.function.VoidFunction2;
 import org.apache.spark.streaming.Durations;
+import org.apache.spark.streaming.Time;
 import org.apache.spark.streaming.api.java.JavaDStream;
 import org.apache.spark.streaming.api.java.JavaInputDStream;
 import org.apache.spark.streaming.api.java.JavaPairDStream;
@@ -40,6 +42,7 @@ public class MoviesHotStatistic {
         try {
             InputStream inStream = MsgConsumer.class.getResourceAsStream("Consumer.properties");
             props.load(inStream);
+            props.put(CommonClientConfigs.BOOTSTRAP_SERVERS_CONFIG, props.getProperty("bootstrap.servers"));
             inStream.close();
         } catch (Exception e) {
             // TODO Auto-generated catch block
@@ -48,27 +51,23 @@ public class MoviesHotStatistic {
     }
 
     public void ExcuteWithDirect(String Topic) throws InterruptedException, ExecutionException {
-        InitProp();
-        Properties properties = new Properties();
-        properties.put(CommonClientConfigs.BOOTSTRAP_SERVERS_CONFIG, props.getProperty("bootstrap.servers"));
-        AdminClient adminClient = AdminClient.create(properties);
-        ListTopicsResult listTopics = adminClient.listTopics();
-        Set<String> names = listTopics.names().get();
-        if(names.contains(Topic)) {
+        Set<String> names = AdminClient.create(props).listTopics().names().get();
+        if (names.contains(Topic)) {
             SparkConf conf = new SparkConf().setMaster("hserver1").setAppName("Demo");
             JavaStreamingContext jsc = new JavaStreamingContext(conf, Durations.seconds(3));
             Collection<String> topic = Arrays.asList(Topic);
             Map<String, Object> kafkaParams = new HashMap<>();
 
-            kafkaParams.put("metadata.broker.list", "hserver1:9092,hserver2:9092,hserver3:9092");
+            kafkaParams.put("metadata.broker.list", props.get("metadata.broker.list"));
+            kafkaParams.put("group.id", props.get("metadata.broker.list") + "_forHotStatistic");
+            kafkaParams.put("auto.offset.reset", props.get("auto.offset.reset"));
+            kafkaParams.put("enable.auto.commit", false);
             kafkaParams.put("key.deserializer", StringDeserializer.class.getName());
             kafkaParams.put("value.deserializer", new BeanDecoder<Rate>().getClass().getName());
-            kafkaParams.put("group.id", "Rate_HotStatistic");
-            kafkaParams.put("auto.offset.reset", "latest");
-            kafkaParams.put("enable.auto.commit", false);
 
             JavaInputDStream<ConsumerRecord<String, Rate>> records = KafkaUtils.createDirectStream(jsc,
-                    LocationStrategies.PreferConsistent(), ConsumerStrategies.<String, Rate>Subscribe(topic, kafkaParams));
+                    LocationStrategies.PreferConsistent(),
+                    ConsumerStrategies.<String, Rate>Subscribe(topic, kafkaParams));
 
             JavaPairDStream<Integer, Tuple2<Double, Integer>> kv = records
                     .mapToPair(new PairFunction<ConsumerRecord<String, Rate>, Integer, Tuple2<Double, Integer>>() {
@@ -99,12 +98,29 @@ public class MoviesHotStatistic {
                         private static final long serialVersionUID = 5808418490663946948L;
 
                         @Override
-                        public Tuple2<Integer, Double> call(Tuple2<Integer, Tuple2<Double, Integer>> t) throws Exception {
+                        public Tuple2<Integer, Double> call(Tuple2<Integer, Tuple2<Double, Integer>> t)
+                                throws Exception {
                             // TODO Auto-generated method stub
                             return new Tuple2<Integer, Double>(t._1, t._2._1 / t._2._1);
                         }
                     });
 
+            MeanByMID.foreachRDD(new VoidFunction<JavaPairRDD<Integer,Double>>() {
+                private static final long serialVersionUID = -2264563798376792125L;
+
+                @Override
+                public void call(JavaPairRDD<Integer, Double> rdd) throws Exception {
+                    // TODO Auto-generated method stub
+                    rdd.foreachPartition(new VoidFunction<Iterator<Tuple2<Integer,Double>>>() {
+                        
+                        @Override
+                        public void call(Iterator<Tuple2<Integer, Double>> t) throws Exception {
+                            // TODO Auto-generated method stub
+                            
+                        }
+                    });
+                }
+            });
             jsc.start();
             jsc.awaitTermination();
             jsc.stop();
